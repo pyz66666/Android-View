e8e0bf72a11d0bf321cbeeeb242ea0bf8112da05
pyz <893758346@qq.com>
2025-03-02 18:41:59 +0800

    自定义 Android消息传递机制

--- pppp/event/src/main/java/com/pyz/event/Activity.java
+++ pppp/event/src/main/java/com/pyz/event/Activity.java
@@ -3,6 +3,7 @@ package com.pyz.event;
 public class Activity {
 
     public static void main(String[] args) {
+        ViewGroup viewGroup = new ViewGroup(0,0,1080,1920);
         MotionEvent motionEvent = new MotionEvent(100,100);
         motionEvent.setActionMasked(MotionEvent.ACTION_DOWN);
     }
--- pppp/event/src/main/java/com/pyz/event/ViewGroup.java
+++ pppp/event/src/main/java/com/pyz/event/ViewGroup.java
@@ -2,11 +2,17 @@ package com.pyz.event;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ViewGroup extends View {
 
     List<View> childList = new ArrayList<>();
     private View[] mChildren = new View[0];//数组的查找比list要快，事件的event是非常的频繁
+    private TouchTarget mFirstTouchTarget;//只给actiondown
+
+    public ViewGroup(int i, int i1, int i2, int i3) {
+        super(i,i1,i2,i3);
+    }
 
     public void addView(View view){
         if (view == null){
@@ -20,6 +26,8 @@ public class ViewGroup extends View {
     public boolean dispatchTouchEvent(MotionEvent ev) {
         //自身是否拦截事件
         boolean intercepted = onInterceptTouchEvent(ev);
+        TouchTarget touchTarget = null;
+        boolean handled = false;
          int actionMasked = ev.getActionMasked();
          if (actionMasked != MotionEvent.ACTION_CANCEL && !intercepted){
              //对down事件进行处理
@@ -35,18 +43,38 @@ public class ViewGroup extends View {
                      //能接收事件的控件
                      if(dispatchTransformTouchEvent(ev,child)){
                         //如果消费   参考handler的obtain message  不要考虑new的方式来创建新的对象
-
+                         handled = true;
+                        touchTarget = addTouchTarget(child);
+                        break;
                      }
                  }
              }
+             if (mFirstTouchTarget == null){
+                 //没有人接收当前事件，调用自己
+                handled =  dispatchTransformTouchEvent(ev, null);
+
+             }
          }
+         return handled;
 
     }
+
+    private TouchTarget addTouchTarget(View child) {
+        final  TouchTarget touchTarget = TouchTarget.obtain(child);
+        touchTarget.next = mFirstTouchTarget;
+        mFirstTouchTarget = touchTarget;
+        return touchTarget;
+    }
+
     //分发处理
     private boolean dispatchTransformTouchEvent(MotionEvent ev, View child) {
         final boolean handled;
         //如果当前的View消费了（设置了点击事件onTouch或者onClick）
-        handled = child.dispatchTouchEvent(ev);
+        if (child != null){
+            handled = child.dispatchTouchEvent(ev);
+        }else {
+            handled = super.dispatchTouchEvent(ev);
+        }
         return handled;
     }
 
@@ -54,5 +82,44 @@ public class ViewGroup extends View {
     public boolean onInterceptTouchEvent(MotionEvent ev) {
 
         return false;
+    }
+
+    /**
+     * 用链表来实现回收池
+     */
+    private static final class TouchTarget{
+        public View child;
+        //回收池
+        public static TouchTarget sRecyclerBin;
+        public TouchTarget next;
+        public static int sRecycledCount;
+        private static final Object sRecyclerLock = new Object();
+        public static TouchTarget obtain(View child){
+            TouchTarget target;
+            synchronized (sRecyclerLock){
+                if (sRecyclerBin == null){
+                    target = new TouchTarget();
+                }else {
+                    target = sRecyclerBin;
+                }
+                sRecyclerBin = target.next;
+                sRecycledCount --;
+                target.next = null;
+            }
+            target.child = child;
+            return target;
+        }
+
+        public void recycle(){
+            synchronized (sRecyclerLock){
+                if (sRecycledCount < 32){
+                    next = sRecyclerBin;
+                    sRecyclerBin = this;
+                    sRecycledCount = 1;
+                }
+            }
+        }
+
+
     }
 }
